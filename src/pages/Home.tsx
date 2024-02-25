6f9ab0533686fbbfeaba64aef3af8b43e240e106
ISAWASHUN <syun20000616@icloud.com>
2024-02-25 22:00:16 +0900

    MonthlySummaryの作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import MonthlySummary from '../components/layout/MonthlySummary'
+import Calendar from '../components/layout/Calendar'
+import TransactionMenu from '../components/layout/TransactionMenu'
+import TransactionForm from '../components/layout/TransactionForm'
+import { pink } from '@mui/material/colors'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box sx={{display: 'flex'}}>
+      {/* 左側のコンテンツ */}
+      <Box sx={{flexGrow: 1, bgcolor: pink}}>
+        <MonthlySummary />
+        <Calendar/>
+      </Box>
+
+      {/* 右側のコンテンツ */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
   )
 }
 
